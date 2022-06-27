3218595f6e3d3ed446b308120fd70fb5e5f50ec9
happydreamman <happydreamman@gmail.com>
2022-06-27 07:48:56 +0000

    complete deploy of saletier

--- scripts/deploySale.js
+++ scripts/deploySale.js
@@ -2,9 +2,10 @@ const hre = require("hardhat");
 const { MerkleTree } = require("merkletreejs");
 const keccak256 = require("keccak256");
 const { bn, ADDRESS_ZERO } = require("../test/utilities");
+const ethers = hre.ethers;
 
-const allocations = `0x41B720be5796ECb7BEB5f712e1cC57dE631240c0,0,0,10,,,
-0x69539C1c678dFd26E626f109149b7cEBDd5E4768,0,0,1000,,,
+const allocations = `0xC61Adc553F01fad7f302C65806693034a91166B8,0,0,10,,,
+0xc3DE43e835Cf52514a21074DE870C056707d4427,0,0,1000,,,
 `;
 
 const parseUnits = ethers.utils.parseUnits;
@@ -47,23 +48,23 @@ async function main() {
   const Contract = await hre.ethers.getContractFactory("SaleTiers");
   const now = (Date.now() / 1000) | 0;
   const args = [
-    "0x2791bca1f2de4661ed88a30c99a7a9449aa84174", // payment token
-    "0x2791bca1f2de4661ed88a30c99a7a9449aa84174", // offering token
+    "0x2791bca1f2de4661ed88a30c99a7a9449aa84174", // payment token  USDC
+    "0x095EE7e82206EAA02741fdDdC07f5bb2Cb3C248c", // offering token DOM
     root, // merkle tree root
-    1651591800, // start time
-    1651678200, // end time
+    1657875600, // start time  2022, 7, 15, 9, 0, 0
+    1659193200, // end time    2022, 7, 30, 15, 0, 0
     parseUnits("25000000"), // offerring amount
-    parseUnits("300000", 6), // raising amount
-    1652542200, // vesting start
+    parseUnits("300000", 6), // raising amount       -----price 0.012USDC
+    1659258000, // vesting start   2022, 7, 31, 9, 0, 0
     parseUnits("0.1", 12), // vesting initial
-    parseUnits("7776000", 0), // vesting duration
+    parseUnits("7776000", 0), // vesting duration    ----90 days
   ];
-  //const contract = await Contract.deploy(...args, {
+  const contract = await Contract.deploy(...args, {
     // gasLimit: 2500000,
     // gasPrice: parseUnits("9000", "gwei")
-  //});
-  //await contract.deployed();
-  const contract = { address: "0x798d0d1716ed93306d7576D595A16658f1Fba31e" };
+  });
+  await contract.deployed();
+  // const contract = { address: "0x798d0d1716ed93306d7576D595A16658f1Fba31e" };
   console.log(contract.address, args);
   if (hre.network.name !== "hardhat") {
     //await new Promise((resolve) => setTimeout(resolve, 30000));
