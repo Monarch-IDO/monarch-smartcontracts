eeb6b7d8135ce3d122b53dff1885fc37565f06b5
happydreamman <happydreamman@gmail.com>
2022-07-02 15:16:11 +0000

    Staking complete

--- scripts/deployStaking.js
+++ scripts/deployStaking.js
@@ -3,27 +3,32 @@ const ethers = hre.ethers;
 
 async function main() {
   //const tokenAddress = "0x69fa0fee221ad11012bab0fdb45d444d3d2ce71c"; // mainnet
-  const tokenAddress = "0x0fe3ecd525d16fa09aa1ff177014de5304c835e2"; // ropsten
+  const tokenAddress = "0xAEDaD96D39C8B24de359417e0de1140cADFAc517"; // ropsten
+  const deployedAddress = "0x973c12E86C3a92402Bc9635A077d18F437Dd42fD"; // ropsten
 
   const signer = await hre.ethers.getSigner();
   const Staking = await hre.ethers.getContractFactory("Staking");
-  const args = [
-    tokenAddress, // token
-    await signer.getAddress(),
-    ethers.utils.parseEther("2")
-  ];
-  const staking = await Staking.deploy(...args);
 
-  await staking.deployed();
-  console.log("Staking deployed to:", staking.address);
+  const staking = Staking.attach(deployedAddress);
 
-  if (hre.network.name !== "hardhat") {
-    await new Promise(resolve => setTimeout(resolve, 20000));
-    await hre.run("verify:verify", {
-      address: staking.address,
-      constructorArguments: args
-    });
-  }
+
+  // const args = [
+  //   tokenAddress, // token
+  //   await signer.getAddress(),
+  //   ethers.utils.parseEther("2")
+  // ];
+  // const staking = await Staking.deploy(...args);
+
+  // await staking.deployed();
+  // console.log("Staking deployed to:", staking.address);
+
+  // if (hre.network.name !== "hardhat") {
+  //   await new Promise(resolve => setTimeout(resolve, 20000));
+  //   await hre.run("verify:verify", {
+  //     address: staking.address,
+  //     constructorArguments: args
+  //   });
+  // }
 
   await staking.add(100, tokenAddress);
   console.log("Staking MONARCH pool added");
